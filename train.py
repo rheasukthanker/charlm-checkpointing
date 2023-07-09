fce8387dbbba89e900d14785bdee4076e32e2373
Rhea Sukthanker <rheasukthanker@gmail.com>
2023-07-09 11:47:15 +0200

    add loading and saving functions

--- train.py
+++ train.py
@@ -3,40 +3,7 @@ import torch.nn as nn
 from torch.nn import functional as F
 from model import CharLM
 import matplotlib.pyplot as plt
-# hyperparameters
-batch_size = 64 # how many independent sequences will we process in parallel?
-block_size = 256 # what is the maximum context length for predictions?
-max_iters = 100
-eval_interval = 10
-learning_rate = 3e-4
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-eval_iters = 200
-n_embd = 384
-n_head = 6
-n_layer = 6
-dropout = 0.2
-# ------------
 
-torch.manual_seed(1337)
-
-# wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
-with open('input.txt', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-# here are all the unique characters that occur in this text
-chars = sorted(list(set(text)))
-vocab_size = len(chars)
-# create a mapping from characters to integers
-stoi = { ch:i for i,ch in enumerate(chars) }
-itos = { i:ch for i,ch in enumerate(chars) }
-encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
-decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
-
-# Train and test splits
-data = torch.tensor(encode(text), dtype=torch.long)
-n = int(0.9*len(data)) # first 90% will be train, rest val
-train_data = data[:n]
-val_data = data[n:]
 
 # data loading
 def get_batch(split):
@@ -68,73 +35,104 @@ def plot_loss(losses):
     plt.ylabel("Loss")
     plt.show()
 
-model = CharLM(vocab_size, n_layer, block_size, n_embd, n_head, dropout=dropout, device=device)
-m = model.to(device)
-# print the number of parameters in the model
-print(sum(p.numel() for p in m.parameters())/1e6, 'M parameters')
-losses_list_val = []
-losses_list_train = []
-# create a PyTorch optimizer
-optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
-scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, max_iters)
-for iter in range(50):
+def save_checkpoint(state_dict, filename):
+    torch.save(state_dict, filename)
 
-    # every once in a while evaluate the loss on train and val sets
-    if iter % eval_interval == 0 or iter == max_iters - 1:
-        losses = estimate_loss()
-        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
-        losses_list_train.append(losses["train"].item())
-        losses_list_val.append(losses["val"].item())
-    # sample a batch of data
-    xb, yb = get_batch('train')
+def load_checkpoint(filename):
+    state_dict = torch.load(filename)
+    return state_dict
 
-    # evaluate the loss
-    logits, loss = model(xb, yb)
-    optimizer.zero_grad(set_to_none=True)
-    loss.backward()
-    optimizer.step()
-    scheduler.step()
-
-# save the model
-state_dict = {"model": model.state_dict(), "optimizer": optimizer.state_dict(), "iter": iter, "scheduler": scheduler.state_dict(), "losses_list_train": losses_list_train, "losses_list_val": losses_list_val}
-model = CharLM(vocab_size, n_layer, block_size, n_embd, n_head, dropout=dropout, device=device)
-m = model.to(device)
-optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
-scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, max_iters)
-# load the model
-state_dict = torch.load("model.pt")
-model.load_state_dict(state_dict["model"])
-optimizer.load_state_dict(state_dict["optimizer"])
-scheduler.load_state_dict(state_dict["scheduler"])
-losses_list_train = state_dict["losses_list_train"]
-losses_list_val = state_dict["losses_list_val"]
-
-iter = state_dict["iter"]
-# Resume training
-for iter in range(iter, max_iters):
-    # every once in a while evaluate the loss on train and val sets
-    if iter % eval_interval == 0 or iter == max_iters - 1:
-        losses = estimate_loss()
-        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
-        losses_list_train.append(losses["train"].item())
-        losses_list_val.append(losses["val"].item())
-
-    # sample a batch of data
-    xb, yb = get_batch('train')
-
-    # evaluate the loss
-    logits, loss = model(xb, yb)
-
-    optimizer.zero_grad(set_to_none=True)
-    loss.backward()
-    optimizer.step()
-    scheduler.step()
-
-# plot the loss curve
-plot_loss(losses_list_train)
-# save plot
-plt.savefig("loss_train.png")
-plt.clf()
-plot_loss(losses_list_val)
-# save plot
-plt.savefig("loss_val.png")
+if __name__ == '__main__':
+    # hyperparameters
+    batch_size = 64 # how many independent sequences will we process in parallel?
+    block_size = 256 # what is the maximum context length for predictions?
+    max_iters = 100
+    eval_interval = 10
+    learning_rate = 3e-4
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    eval_iters = 200
+    n_embd = 384
+    n_head = 6
+    n_layer = 6
+    dropout = 0.2
+    # ------------
+    torch.manual_seed(1337)
+    # wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
+    with open('input.txt', 'r', encoding='utf-8') as f:
+        text = f.read()
+    # here are all the unique characters that occur in this text
+    chars = sorted(list(set(text)))
+    vocab_size = len(chars)
+    # create a mapping from characters to integers
+    stoi = { ch:i for i,ch in enumerate(chars) }
+    itos = { i:ch for i,ch in enumerate(chars) }
+    encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
+    decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string# Train and test splits
+    data = torch.tensor(encode(text), dtype=torch.long)
+    n = int(0.9*len(data)) # first 90% will be train, rest val
+    train_data = data[:n]
+    val_data = data[n:]
+    print(vocab_size, n_layer, block_size, n_embd, n_head, dropout, device)
+    model = CharLM(vocab_size, n_layer, block_size, n_embd, n_head, dropout=dropout, device=device)
+    m = model.to(device)
+    # print the number of parameters in the model
+    print(sum(p.numel() for p in m.parameters())/1e6, 'M parameters')
+    losses_list_val = []
+    losses_list_train = []
+    # create a PyTorch optimizer
+    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, max_iters)
+    for iter in range(50):
+        # every once in a while evaluate the loss on train and val sets
+        if iter % eval_interval == 0 or iter == max_iters - 1:
+            losses = estimate_loss()
+            print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+            losses_list_train.append(losses["train"].item())
+            losses_list_val.append(losses["val"].item())
+        # sample a batch of data
+        xb, yb = get_batch('train')
+        # evaluate the loss
+        logits, loss = model(xb, yb)
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+    # save the model
+    state_dict = {"model": model.state_dict(), "optimizer": optimizer.state_dict(), "iter": iter, "scheduler": scheduler.state_dict(), "losses_list_train": losses_list_train, "losses_list_val": losses_list_val}
+    save_checkpoint(state_dict, "model.pt")
+    model = CharLM(vocab_size, n_layer, block_size, n_embd, n_head, dropout=dropout, device=device)
+    m = model.to(device)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, max_iters)
+    # load the model
+    state_dict = load_checkpoint("model.pt")
+    model.load_state_dict(state_dict["model"])
+    optimizer.load_state_dict(state_dict["optimizer"])
+    scheduler.load_state_dict(state_dict["scheduler"])
+    losses_list_train = state_dict["losses_list_train"]
+    losses_list_val = state_dict["losses_list_val"]
+    iter = state_dict["iter"]
+    # Resume training
+    for iter in range(iter, max_iters):
+        # every once in a while evaluate the loss on train and val sets
+        if iter % eval_interval == 0 or iter == max_iters - 1:
+            losses = estimate_loss()
+            print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+            losses_list_train.append(losses["train"].item())
+            losses_list_val.append(losses["val"].item())
+            # sample a batch of data
+        xb, yb = get_batch('train')
+        # evaluate the loss
+        logits, loss = model(xb, yb)
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+    # plot the loss curve
+    plot_loss(losses_list_train)
+    # save plot
+    plt.savefig("loss_train.png")
+    plt.clf()
+    plot_loss(losses_list_val)
+    # save plot
+    plt.savefig("loss_val.png")
